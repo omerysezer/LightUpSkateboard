cfe232a218df038164b1b670d7b800dd1602d7f1
OmerYSezer <omerysezer@gmail.com>
2023-05-06 17:15:15 -0400

    added light test

---
+++ light_test/light_test.ino
@@ -0,0 +1,44 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+
+#define NUM_PIXELS 5000
+#define PIN_NEO_PIXEL 2
+Adafruit_MPU6050 mpu;
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_PIXELS, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixel2 = Adafruit_NeoPixel(1, 25, NEO_GRB + NEO_KHZ800);
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  pixel2.begin();
+  pixel2.setBrightness(32);
+  pixel2.setPixelColor(0, pixel2.Color(255, 255, 255));
+  pixel2.show();
+
+  pixel.begin();
+  for(int i = 0; i < NUM_PIXELS; i++){
+    pixel.setPixelColor(i, pixel.Color(0,0,0));
+  }
+  pixel.setBrightness(32);
+  pixel.show();
+
+  int r[7] = {255, 255, 255, 0, 0, 75, 148};
+  int g[7] = {0, 127, 255, 255, 0, 0, 0};
+  int b[7] = {0, 0, 0, 0, 255, 130, 211};
+  for(int i = 0; i < NUM_PIXELS; i++){
+    int x = i%7;
+    pixel.setPixelColor(i, pixel.Color(r[x], g[x], b[x]));
+  }
+  delay(100);
+}
+
+
+void loop() {
+  pixel.show();
+}
